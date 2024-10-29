d15b6a27873127e51301574549b3f8a8f2c5fe3e
mathdeck <52067259+mathdeck@users.noreply.github.com>
2024-10-29 20:00:21 +0100

    Update Extracteur_Météo.sh

--- Extracteur_Météo.sh
+++ Extracteur_Météo.sh
@@ -1,17 +1,35 @@
-#!/bin/bash
+##################User 1 ###################
 
-echo "Veuillez entrer le nom d'une ville :"
-read nom_ville
+#! /bin/sh
+#vérification nombre de paramètres
+if [ $# -eq 0 ]
+then
+    ville="Toulouse"
+elif [ $# -ne 1 ]
+then
+    echo "Usage : ./scriptv2.sh <Ville>"
+    exit 1
+else
+    ville=$1
+fi
 
-#Recup de la meteo actuelle
-temperature_aujourdhui=$(curl -s "wttr.in/$nom_ville?format=%t")
+#exportation données brutes dans fichier .txt
+curl -s wttr.in/$ville?format=j2 > local.txt
 
-#Recup prevision demain
-temperature_demain=$(curl -s "wttr.in/$nom_ville?format=%t&tomorrow")
+#récupération température actuelle
+tempact=$(curl -s wttr.in/$ville?format="%t")
+if [ "${tempact:0:1}" = "+" ]
+then
+    tempact=$(echo $tempact | cut -c 2-)
+fi
 
-#Date et heure 
-jour_actuel=$(date '+%Y-%m-%d')
-heure_actuelle=$(date '+%H:%M')
+#récupération température du lendemain
+templen=$(head -n 103 local.txt | tail -n 1 | grep -oE "\-*[0-9]*")°C
 
-#Enregistrer les infos 
-echo "$jour_actuel - $heure_actuelle - $nom_ville : $temperature_aujourdhui - $temperature_demain" >> meteo.txt
+#meteo en une ligne
+heure=$(date +"%H:%M")
+date=$(date +"%Y-%m-%d")
+meteo="$date - $heure - $ville : $tempact - $templen"
+echo $meteo >> meteo.txt
+
+exit 0
